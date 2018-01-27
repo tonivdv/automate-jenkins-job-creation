3d5e7c8b02cd7d1a40f765f14862fdac5c2145f7
Toni Van de Voorde <toni.vdv@gmail.com>
2018-01-27 07:32:53 +0100

    fix job not in folder

--- demo-dsl-files/demo5bis.groovy
+++ demo-dsl-files/demo5bis.groovy
@@ -10,7 +10,7 @@ folder('demo5bis')
 branches.each {
 
     def branchName = it
-    def jobName = "demo5bis-${project}-${branchName}".replaceAll('/','-')
+    def jobName = "demo5bis/demo5bis-${project}-${branchName}".replaceAll('/','-')
     job(jobName) {
         scm {
             git("https://github.com/${project}.git", branchName)
